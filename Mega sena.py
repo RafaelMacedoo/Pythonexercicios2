3e9b1bb9314eb5626ee598d8aec7ac52a4383bd4
Rafael Augusto Silva Macedo Menezes <232424692+RafaelMacedoo@users.noreply.github.com>
2026-08-10 17:20:37 -0300

    Exercício de número 88, criação de palpites para jogos da mega sena.

---
+++ Mega sena.py
@@ -0,0 +1,20 @@
+import random
+lista = []
+jogos = []
+quant = int(input('Quantos jogos você quer? '))
+tot = 1
+while tot <= quant:
+    cont = 0
+    while True:
+        num = random.randint(1,60)
+        if num not in lista:
+            lista.append(num)
+            cont += 1
+        if cont >= 6:
+            break
+    lista.sort()
+    jogos.append(lista[:])
+    lista.clear()
+    tot += 1
+for i, l in enumerate(jogos):
+    print(f'Jogo {i+1}: {l}')
